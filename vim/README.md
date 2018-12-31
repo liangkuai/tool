1c56d494fad6fe285624465ee7de8cbb188119fd
liukai <liangkuai.lk@gmail.com>
2019-01-01 00:50:26 +0800

    vim
    README

---
+++ vim/README.md
@@ -0,0 +1,9 @@
+
+### Vundle 常用插件
+
+#### 1. 状态栏：[vim-airline](https://github.com/vim-airline/vim-airline)
+
+```
+Plugin 'vim-airline/vim-airline'
+Plugin 'vim-airline/vim-airline-themes'
+```
--- vim/config.sh
+++ vim/config.sh
@@ -12,21 +12,21 @@ if [ -z "$install_vundle" ] || [ "$install_vundle" = "y" -o "$instanll_vundle" =
 
     echo '2. 配置到 ~/.vimrc'
     echo -e '\n\n\n" Vundle 配置开始' >> ~/.vimrc
-    echo 'set nocompatible                  " 必需' >> ~/.vimrc
-    echo 'filetype off                      " 必需' >> ~/.vimrc
+    echo 'set nocompatible                  " 必须' >> ~/.vimrc
+    echo 'filetype off                      " 必须' >> ~/.vimrc
 
     echo -e '\n" 设置运行时路径以包括Vundle和初始化' >> ~/.vimrc
     echo 'set rtp+=~/.vim/bundle/Vundle.vim' >> ~/.vimrc
     echo 'call vundle#begin()' >> ~/.vimrc
 
-    echo -e '\n" 必需，使用Vundle管理Vundle' >> ~/.vimrc
+    echo -e '\n" 必须，使用Vundle管理Vundle' >> ~/.vimrc
     echo "Plugin 'VundleVim/Vundle.vim'" >> ~/.vimrc
 
     echo -e '\n" 添加插件到此处' >> ~/.vimrc
 
     echo -e '\n' >> ~/.vimrc
-    echo 'call vundle#end()                    "必需' >> ~/.vimrc
-    echo 'filetype plugin indent on            "必需' >> ~/.vimrc
+    echo 'call vundle#end()                    "必须' >> ~/.vimrc
+    echo 'filetype plugin indent on            "必须，加载vim自带和插件相应的语法和文件类型相关脚本' >> ~/.vimrc
     echo '"Vundle 配置结束' >> ~/.vimrc
 
     vim +PluginInstall +qall
